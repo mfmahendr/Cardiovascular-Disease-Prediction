f5ba2997cb3101550a874208b325a3112e91f343
M. Fadhil Mahendra <71614957+mfmahendr@users.noreply.github.com>
2022-09-10 15:04:21 +0700

    Upload Files

---
+++ CardiovascularAPITest/Input.Designer.cs
@@ -0,0 +1,408 @@
+﻿
+namespace CardiovascularAPITest
+{
+    partial class Input
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.formLabel = new System.Windows.Forms.Label();
+            this.ageInputLabel = new System.Windows.Forms.Label();
+            this.genderInputLabel = new System.Windows.Forms.Label();
+            this.heightInputLabel = new System.Windows.Forms.Label();
+            this.weightInputLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cholesterolInputLabel = new System.Windows.Forms.Label();
+            this.glucInputLabel = new System.Windows.Forms.Label();
+            this.smokeInputLabel = new System.Windows.Forms.Label();
+            this.alcoInputLabel = new System.Windows.Forms.Label();
+            this.activeInputLabel = new System.Windows.Forms.Label();
+            this.submit = new System.Windows.Forms.Button();
+            this.alcoInput = new System.Windows.Forms.GroupBox();
+            this.genderInput = new System.Windows.Forms.NumericUpDown();
+            this.activeInput = new System.Windows.Forms.NumericUpDown();
+            this.alcoholInput = new System.Windows.Forms.NumericUpDown();
+            this.smokeInput = new System.Windows.Forms.NumericUpDown();
+            this.glucInput = new System.Windows.Forms.NumericUpDown();
+            this.cholesterolInput = new System.Windows.Forms.NumericUpDown();
+            this.apLowInput = new System.Windows.Forms.NumericUpDown();
+            this.ageInput = new System.Windows.Forms.NumericUpDown();
+            this.apHighInput = new System.Windows.Forms.NumericUpDown();
+            this.weightInput = new System.Windows.Forms.NumericUpDown();
+            this.heightInput = new System.Windows.Forms.NumericUpDown();
+            this.alcoInput.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.genderInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.activeInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.alcoholInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.smokeInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.glucInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cholesterolInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.apLowInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ageInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.apHighInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.weightInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heightInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // formLabel
+            // 
+            this.formLabel.AutoSize = true;
+            this.formLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.formLabel.Location = new System.Drawing.Point(235, 23);
+            this.formLabel.Name = "formLabel";
+            this.formLabel.Size = new System.Drawing.Size(193, 28);
+            this.formLabel.TabIndex = 1;
+            this.formLabel.Text = "Input data to predict";
+            this.formLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ageInputLabel
+            // 
+            this.ageInputLabel.AutoSize = true;
+            this.ageInputLabel.Location = new System.Drawing.Point(203, 93);
+            this.ageInputLabel.Name = "ageInputLabel";
+            this.ageInputLabel.Size = new System.Drawing.Size(39, 20);
+            this.ageInputLabel.TabIndex = 3;
+            this.ageInputLabel.Text = "Age:";
+            // 
+            // genderInputLabel
+            // 
+            this.genderInputLabel.AutoSize = true;
+            this.genderInputLabel.Location = new System.Drawing.Point(195, 146);
+            this.genderInputLabel.Name = "genderInputLabel";
+            this.genderInputLabel.Size = new System.Drawing.Size(60, 20);
+            this.genderInputLabel.TabIndex = 5;
+            this.genderInputLabel.Text = "Gender:";
+            // 
+            // heightInputLabel
+            // 
+            this.heightInputLabel.AutoSize = true;
+            this.heightInputLabel.Location = new System.Drawing.Point(194, 196);
+            this.heightInputLabel.Name = "heightInputLabel";
+            this.heightInputLabel.Size = new System.Drawing.Size(57, 20);
+            this.heightInputLabel.TabIndex = 7;
+            this.heightInputLabel.Text = "Height:";
+            // 
+            // weightInputLabel
+            // 
+            this.weightInputLabel.AutoSize = true;
+            this.weightInputLabel.Location = new System.Drawing.Point(192, 246);
+            this.weightInputLabel.Name = "weightInputLabel";
+            this.weightInputLabel.Size = new System.Drawing.Size(59, 20);
+            this.weightInputLabel.TabIndex = 9;
+            this.weightInputLabel.Text = "Weight:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(194, 292);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "AP HIGH:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(192, 341);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "AP LOW:";
+            // 
+            // cholesterolInputLabel
+            // 
+            this.cholesterolInputLabel.AutoSize = true;
+            this.cholesterolInputLabel.Location = new System.Drawing.Point(194, 395);
+            this.cholesterolInputLabel.Name = "cholesterolInputLabel";
+            this.cholesterolInputLabel.Size = new System.Drawing.Size(87, 20);
+            this.cholesterolInputLabel.TabIndex = 15;
+            this.cholesterolInputLabel.Text = "Cholesterol:";
+            // 
+            // glucInputLabel
+            // 
+            this.glucInputLabel.AutoSize = true;
+            this.glucInputLabel.Location = new System.Drawing.Point(193, 443);
+            this.glucInputLabel.Name = "glucInputLabel";
+            this.glucInputLabel.Size = new System.Drawing.Size(64, 20);
+            this.glucInputLabel.TabIndex = 17;
+            this.glucInputLabel.Text = "Glucose:";
+            // 
+            // smokeInputLabel
+            // 
+            this.smokeInputLabel.AutoSize = true;
+            this.smokeInputLabel.Location = new System.Drawing.Point(197, 489);
+            this.smokeInputLabel.Name = "smokeInputLabel";
+            this.smokeInputLabel.Size = new System.Drawing.Size(57, 20);
+            this.smokeInputLabel.TabIndex = 19;
+            this.smokeInputLabel.Text = "Smoke:";
+            // 
+            // alcoInputLabel
+            // 
+            this.alcoInputLabel.AutoSize = true;
+            this.alcoInputLabel.Location = new System.Drawing.Point(197, 536);
+            this.alcoInputLabel.Name = "alcoInputLabel";
+            this.alcoInputLabel.Size = new System.Drawing.Size(63, 20);
+            this.alcoInputLabel.TabIndex = 21;
+            this.alcoInputLabel.Text = "Alcohol:";
+            // 
+            // activeInputLabel
+            // 
+            this.activeInputLabel.AutoSize = true;
+            this.activeInputLabel.Location = new System.Drawing.Point(200, 579);
+            this.activeInputLabel.Name = "activeInputLabel";
+            this.activeInputLabel.Size = new System.Drawing.Size(53, 20);
+            this.activeInputLabel.TabIndex = 23;
+            this.activeInputLabel.Text = "Active:";
+            // 
+            // submit
+            // 
+            this.submit.Location = new System.Drawing.Point(273, 627);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(94, 29);
+            this.submit.TabIndex = 2;
+            this.submit.Text = "Predict";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // alcoInput
+            // 
+            this.alcoInput.Controls.Add(this.genderInput);
+            this.alcoInput.Controls.Add(this.activeInput);
+            this.alcoInput.Controls.Add(this.alcoholInput);
+            this.alcoInput.Controls.Add(this.smokeInput);
+            this.alcoInput.Controls.Add(this.glucInput);
+            this.alcoInput.Controls.Add(this.cholesterolInput);
+            this.alcoInput.Controls.Add(this.apLowInput);
+            this.alcoInput.Controls.Add(this.ageInput);
+            this.alcoInput.Controls.Add(this.apHighInput);
+            this.alcoInput.Controls.Add(this.weightInput);
+            this.alcoInput.Controls.Add(this.heightInput);
+            this.alcoInput.Controls.Add(this.submit);
+            this.alcoInput.Controls.Add(this.activeInputLabel);
+            this.alcoInput.Controls.Add(this.alcoInputLabel);
+            this.alcoInput.Controls.Add(this.smokeInputLabel);
+            this.alcoInput.Controls.Add(this.glucInputLabel);
+            this.alcoInput.Controls.Add(this.cholesterolInputLabel);
+            this.alcoInput.Controls.Add(this.label4);
+            this.alcoInput.Controls.Add(this.label3);
+            this.alcoInput.Controls.Add(this.weightInputLabel);
+            this.alcoInput.Controls.Add(this.heightInputLabel);
+            this.alcoInput.Controls.Add(this.genderInputLabel);
+            this.alcoInput.Controls.Add(this.ageInputLabel);
+            this.alcoInput.Controls.Add(this.formLabel);
+            this.alcoInput.Location = new System.Drawing.Point(65, 38);
+            this.alcoInput.Name = "alcoInput";
+            this.alcoInput.Size = new System.Drawing.Size(673, 672);
+            this.alcoInput.TabIndex = 1;
+            this.alcoInput.TabStop = false;
+            // 
+            // genderInput
+            // 
+            this.genderInput.Location = new System.Drawing.Point(373, 144);
+            this.genderInput.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.genderInput.Name = "genderInput";
+            this.genderInput.Size = new System.Drawing.Size(150, 27);
+            this.genderInput.TabIndex = 36;
+            // 
+            // activeInput
+            // 
+            this.activeInput.Location = new System.Drawing.Point(373, 577);
+            this.activeInput.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.activeInput.Name = "activeInput";
+            this.activeInput.Size = new System.Drawing.Size(150, 27);
+            this.activeInput.TabIndex = 35;
+            // 
+            // alcoholInput
+            // 
+            this.alcoholInput.Location = new System.Drawing.Point(373, 534);
+            this.alcoholInput.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.alcoholInput.Name = "alcoholInput";
+            this.alcoholInput.Size = new System.Drawing.Size(150, 27);
+            this.alcoholInput.TabIndex = 34;
+            // 
+            // smokeInput
+            // 
+            this.smokeInput.Location = new System.Drawing.Point(373, 482);
+            this.smokeInput.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.smokeInput.Name = "smokeInput";
+            this.smokeInput.Size = new System.Drawing.Size(150, 27);
+            this.smokeInput.TabIndex = 33;
+            // 
+            // glucInput
+            // 
+            this.glucInput.Location = new System.Drawing.Point(373, 436);
+            this.glucInput.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.glucInput.Name = "glucInput";
+            this.glucInput.Size = new System.Drawing.Size(150, 27);
+            this.glucInput.TabIndex = 32;
+            // 
+            // cholesterolInput
+            // 
+            this.cholesterolInput.Location = new System.Drawing.Point(373, 388);
+            this.cholesterolInput.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.cholesterolInput.Name = "cholesterolInput";
+            this.cholesterolInput.Size = new System.Drawing.Size(150, 27);
+            this.cholesterolInput.TabIndex = 31;
+            // 
+            // apLowInput
+            // 
+            this.apLowInput.Location = new System.Drawing.Point(373, 334);
+            this.apLowInput.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.apLowInput.Name = "apLowInput";
+            this.apLowInput.Size = new System.Drawing.Size(150, 27);
+            this.apLowInput.TabIndex = 30;
+            // 
+            // ageInput
+            // 
+            this.ageInput.Location = new System.Drawing.Point(373, 91);
+            this.ageInput.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.ageInput.Name = "ageInput";
+            this.ageInput.Size = new System.Drawing.Size(150, 27);
+            this.ageInput.TabIndex = 29;
+            // 
+            // apHighInput
+            // 
+            this.apHighInput.Location = new System.Drawing.Point(373, 285);
+            this.apHighInput.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.apHighInput.Name = "apHighInput";
+            this.apHighInput.Size = new System.Drawing.Size(150, 27);
+            this.apHighInput.TabIndex = 28;
+            // 
+            // weightInput
+            // 
+            this.weightInput.Location = new System.Drawing.Point(373, 239);
+            this.weightInput.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.weightInput.Name = "weightInput";
+            this.weightInput.Size = new System.Drawing.Size(150, 27);
+            this.weightInput.TabIndex = 27;
+            // 
+            // heightInput
+            // 
+            this.heightInput.Location = new System.Drawing.Point(373, 194);
+            this.heightInput.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.heightInput.Name = "heightInput";
+            this.heightInput.Size = new System.Drawing.Size(150, 27);
+            this.heightInput.TabIndex = 26;
+            // 
+            // Input
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 744);
+            this.Controls.Add(this.alcoInput);
+            this.Name = "Input";
+            this.Text = "Input Data Form";
+            this.alcoInput.ResumeLayout(false);
+            this.alcoInput.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.genderInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.activeInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.alcoholInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.smokeInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.glucInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cholesterolInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.apLowInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ageInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.apHighInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.weightInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heightInput)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label formLabel;
+        private System.Windows.Forms.Label ageInputLabel;
+        private System.Windows.Forms.Label genderInputLabel;
+        private System.Windows.Forms.Label heightInputLabel;
+        private System.Windows.Forms.Label weightInputLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label cholesterolInputLabel;
+        private System.Windows.Forms.Label glucInputLabel;
+        private System.Windows.Forms.Label smokeInputLabel;
+        private System.Windows.Forms.Label alcoInputLabel;
+        private System.Windows.Forms.Label activeInputLabel;
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.GroupBox alcoInput;
+        private System.Windows.Forms.NumericUpDown apHighInput;
+        private System.Windows.Forms.NumericUpDown weightInput;
+        private System.Windows.Forms.NumericUpDown heightInput;
+        private System.Windows.Forms.NumericUpDown genderInput;
+        private System.Windows.Forms.NumericUpDown activeInput;
+        private System.Windows.Forms.NumericUpDown alcoholInput;
+        private System.Windows.Forms.NumericUpDown smokeInput;
+        private System.Windows.Forms.NumericUpDown glucInput;
+        private System.Windows.Forms.NumericUpDown cholesterolInput;
+        private System.Windows.Forms.NumericUpDown apLowInput;
+        private System.Windows.Forms.NumericUpDown ageInput;
+    }
+}
+
---
+++ CardiovascularAPITest/Input.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Collections.Generic;
+// This code requires the Nuget package Microsoft.AspNet.WebApi.Client to be installed.
+// Instructions for doing this in Visual Studio:
+// Tools -> Nuget Package Manager -> Package Manager Console
+// Install-Package Newtonsoft.Json
+// .NET Framework 4.7.1 or greater must be used
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace CardiovascularAPITest
+{
+    public partial class Input : Form
+    {
+
+
+        public Input()
+        {
+            InitializeComponent();
+
+        }
+
+        static async Task InvokeRequestResponseService(string scoreRequestContent)
+        {
+            var handler = new HttpClientHandler()
+            {
+                ClientCertificateOptions = ClientCertificateOption.Manual,
+                ServerCertificateCustomValidationCallback =
+                        (httpRequestMessage, cert, cetChain, policyErrors) => { return true; }
+            };
+            using (var client = new HttpClient(handler))
+            {
+                
+                
+                const string apiKey = ""; // Replace this with the API key for the web service
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+                client.BaseAddress = new Uri("http://2d54029f-e792-43be-b96b-68efadbf489a.southeastasia.azurecontainer.io/score");
+
+                var content = new StringContent(scoreRequestContent);
+
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                // WARNING: The 'await' statement below can result in a deadlock
+                // if you are calling this code from the UI thread of an ASP.Net application.
+                // One way to address this would be to call ConfigureAwait(false)
+                // so that the execution does not attempt to resume on the original context.
+                // For instance, replace code such as:
+                //      result = await DoSomeTask()
+                // with the following:
+                //      result = await DoSomeTask().ConfigureAwait(false)
+                HttpResponseMessage response = await client.PostAsync("", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    MessageBox.Show("Result: " + result);
+                }
+                else
+                {
+                    MessageBox.Show(string.Format("The request failed with status code: {0}", response.StatusCode));
+
+                    // Print the headers - they include the requert ID and the timestamp,
+                    // which are useful for debugging the failure
+                    MessageBox.Show(response.Headers.ToString());
+
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    MessageBox.Show(responseContent);
+                }
+            }
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            int id = 100;
+
+
+            // Request data goes here
+            // The example below assumes JSON formatting which may be updated
+            // depending on the format your endpoint expects.
+            // More information can be found here:
+            // https://docs.microsoft.com/azure/machine-learning/how-to-deploy-advanced-entry-script
+            var scoreRequestContent = @"{
+                  ""Inputs"": {
+                    ""data"": [
+                      {
+                        ""id"": """ + id + @""",
+                        ""age"": """ + this.ageInput.Value + @""",
+                        ""gender"": """ + this.genderInput.Text + @""",
+                        ""height"": """ + this.heightInput.Value + @""",
+                        ""weight"": """ + this.weightInput.Value + @""",
+                        ""ap_hi"": """ + this.apHighInput.Value + @""",
+                        ""ap_lo"": """ + this.apLowInput.Value + @""",
+                        ""cholesterol"": """ + this.cholesterolInput.Value + @""",
+                        ""gluc"": """ + this.glucInput.Value + @""",
+                        ""smoke"": """ + this.smokeInput.Value + @""",
+                        ""alco"": """ + this.alcoholInput.Value + @""",
+                        ""active"": """ + this.activeInput.Value + @"""
+                      }
+                    ]
+                  },
+                  ""GlobalParameters"": {
+                    ""method"": ""predict""
+                  }
+                }";
+            MessageBox.Show(scoreRequestContent);
+            _ = InvokeRequestResponseService(scoreRequestContent);
+        }
+
+       
+    }
+    
+}
